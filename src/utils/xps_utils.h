8a14db6703ca194ed6f3302ef43b23ff9ef689ee
GOKULKRISHNA <107233184+Ryzertron@users.noreply.github.com>
2026-04-07 12:04:46 +0530

    Project Setup complete | one possible error in buffer.c

---
+++ src/utils/xps_utils.h
@@ -0,0 +1,15 @@
+#ifndef XPS_UTILS_H
+#define XPS_UTILS_H
+
+#include "../xps.h"
+#include "xps_logger.h"
+
+// Sockets
+bool is_valid_port(u_int port);
+int make_socket_non_blocking(u_int sock_fd);
+struct addrinfo *xps_getaddrinfo(const char *host, u_int port);
+char *get_remote_ip(u_int sock_fd);
+
+// Other functions
+
+#endif
